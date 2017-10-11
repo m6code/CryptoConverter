1e39f1c23e510be793469d0e42f1b0c10282e691
Benjamin M6 <benjamynfrank101@gmail.com>
2017-10-11 08:59:42 -0500

    Load coin - money conversion data from the Crypto compare Api

--- app/src/main/java/com/m6code/cryptocurrencyconverter/MainActivity.java
+++ app/src/main/java/com/m6code/cryptocurrencyconverter/MainActivity.java
@@ -1,10 +1,18 @@
 package com.m6code.cryptocurrencyconverter;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,35 +21,85 @@ public class MainActivity extends AppCompatActivity {
     public static final String LOG_TAG = MainActivity.class.getName();
 
 
-//    /**
-//     * URL for coin data from Cryptocompare API
-//     */
-//    private static final String COIN_DATA_REQUEST_URL =
-//            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC";
+    /**
+     * URL for coin data from Cryptocompare API
+     */
+    private static final String COIN_API_URL_MULTI =
+            "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD," +
+                    "EUR,NGN,JPY,GPB,CHF,CAD,AUD,HKD,BRL,INR,KRW,ILS,RUB,ZAR,SEK,PHP,CNY,TRY";
+
+    //private static final String API_URL = "https://min-api.cryptocompare.com";
+
+    /**
+     * Base url for getting images of coins
+     */
+    private static final String COIN_BASE_URL = "https://cryptocompare.com";
+
+    ArrayList<Coins> coins = new ArrayList<Coins>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        ArrayList<Coins> coins = new ArrayList<Coins>();
-        coins.add(new Coins(R.mipmap.ic_launcher, "BTC", "4524", "NGR"));
-        coins.add(new Coins(R.mipmap.ic_launcher_round, "ETH", "453", "USD"));
-        coins.add(new Coins(R.mipmap.ic_launcher, "BTC", "987", "USD"));
-        coins.add(new Coins(R.mipmap.ic_launcher_round, "ETH","768", "NGR"));
-        coins.add(new Coins(R.mipmap.ic_launcher, "BTC", "123", "INR"));
-        coins.add(new Coins(R.mipmap.ic_launcher_round, "ETH", "445", "JPY"));
+        OkHttpHandler okHttpHandler = new OkHttpHandler();
+        okHttpHandler.execute(COIN_API_URL_MULTI);
 
 
-        // Find a reference to the {@link ListView} from the activity_main layout
-        ListView userListView = (ListView)findViewById(R.id.list);
+    }
 
-        // Create a new adapter that takes an empty list of users as input
-        CoinsAdapter adapter = new CoinsAdapter(this, coins);
+    private class OkHttpHandler extends AsyncTask<String, Void, String> {
+        OkHttpClient client = new OkHttpClient();
 
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        userListView.setAdapter(adapter);
+        @Override
+        protected String doInBackground(String... url) {
+            Request.Builder builder = new Request.Builder();
+            builder.url(url[0]);
+            Request request = builder.build();
+
+            try{
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+
+            try{
+                JSONObject multiCoinDataObject = new JSONObject(data);
+
+                JSONObject btcCoinData = multiCoinDataObject.getJSONObject("BTC");
+
+                JSONObject ethCoinData = multiCoinDataObject.getJSONObject("ETH");
+
+
+                // Extract conversion rate for BTC to Naira
+                String btc2naira = btcCoinData.getString("NGN");
+                coins.add(new Coins(R.mipmap.ic_launcher, "1 BTC", btc2naira, "NGN"));
+
+                // Extract conversion rate for ETH to naira
+                String eth2naira = ethCoinData.getString("NGN");
+                coins.add(new Coins(R.mipmap.ic_launcher, "1 ETH",eth2naira, "NGN"));
+
+                // Find a reference to the {@link ListView} from the activity_main layout
+                ListView userListView = (ListView)findViewById(R.id.list);
+
+                // Create a new adapter that takes an empty list of users as input
+                CoinsAdapter adapter = new CoinsAdapter(MainActivity.this, coins);
+
+                // Set the adapter on the {@link ListView}
+                // so the list can be populated in the user interface
+                userListView.setAdapter(adapter);
+
+            }catch(JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
