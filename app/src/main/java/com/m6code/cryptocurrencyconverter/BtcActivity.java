084b6c7048d72148123711c12df3d4bc3e70d8cd
Benjamin M6 <benjamynfrank101@gmail.com>
2017-11-09 04:00:43 -0600

    Add onOptionItemSelected listener to launch items activity when clicked

--- app/src/main/java/com/m6code/cryptocurrencyconverter/BtcActivity.java
+++ app/src/main/java/com/m6code/cryptocurrencyconverter/BtcActivity.java
@@ -6,12 +6,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class BtcActivity extends AppCompatActivity{
+public class BtcActivity extends AppCompatActivity {
 
 
     // create the log tag to log errors to console
@@ -40,12 +42,11 @@ public class BtcActivity extends AppCompatActivity{
         // so the list can be populated in the user interface
         userListView.setAdapter(adapter);
 
-        FloatingActionButton fab_eth = (FloatingActionButton)findViewById(R.id.fab_eth);
+        FloatingActionButton fab_eth = (FloatingActionButton) findViewById(R.id.fab_eth);
         fab_eth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ethLaunch = new Intent(BtcActivity.this, EthActivity.class);
-                startActivity(ethLaunch);
+                launchETH();
             }
         });
 
@@ -60,5 +61,30 @@ public class BtcActivity extends AppCompatActivity{
         menuInflater.inflate(R.menu.activity_main_actions, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    // Handles on clicking menu option
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btc_view:
+                Toast.makeText(this,"Already on BTC", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.eth_view:
+                launchETH();
+                return true;
+            case R.id.convert:
+                //TODO: Add the code to launch the convert activity here
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Launch The ETH Activity
+    private void launchETH() {
+        Intent ethLaunch = new Intent(this, EthActivity.class);
+        startActivity(ethLaunch);
+    }
+
 
 }
--- app/src/main/java/com/m6code/cryptocurrencyconverter/EthActivity.java
+++ app/src/main/java/com/m6code/cryptocurrencyconverter/EthActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class EthActivity extends AppCompatActivity {
 
@@ -35,6 +37,30 @@ public class EthActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.activity_main_actions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    // Handles on clicking menu option
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.eth_view:
+                Toast.makeText(this,"Already on ETH", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.btc_view:
+                launchBTC();
+                return true;
+            case R.id.convert:
+                //TODO: Add the code to launch the convert activity here
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Launch The BTC Activity
+    private void launchBTC() {
+        Intent ethLaunch = new Intent(this, BtcActivity.class);
+        startActivity(ethLaunch);
     }
 
 
